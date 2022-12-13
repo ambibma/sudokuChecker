90f1f6d82c17d668c7c15e9b083c958ec2b8be68
ambi hidalgo <ambio.pk@gmail.com>
2022-12-12 20:37:08 -0800

    Test 2 and 3 pass, returns true for given array that does not contain dup'd numbers, and false if does
    Co-authored-by: Cameron Abel <cameronabel@gmail.com>
    Co-authored-by: Geoffrey Gao <geoffreygao1@gmail.com>”

--- __tests__/sudoku.test.js
+++ __tests__/sudoku.test.js
@@ -8,21 +8,21 @@ describe('Sudoku', () => {
       expect(row.length).toEqual(9);
     });
   });
-  
-  test('it should return true if the given row does not contain duplicate numbers', () =>{ 
+
+  test('it should return true if the given array does not contain duplicate numbers', () =>{ 
     const sudoku = new Sudoku();
     sudoku.grid[0] = ['1','2','3','4','5','6','7','8','9'];
     sudoku.grid[1] = [,'1','3','4','5','6','7','8','9'];
-    expect(sudoku.validateRow(0)).toEqual(true);
-    expect(sudoku.validateRow(1)).toEqual(true);
+    expect(sudoku.validateCollection(sudoku.grid[0])).toEqual(true);
+    expect(sudoku.validateCollection(sudoku.grid[1])).toEqual(true);
   });
 
-  test('it should return false if the given row does contain duplicate numbers', () =>{ 
+  test('it should return false if the given array does contain duplicate numbers', () =>{ 
     const sudoku = new Sudoku();
     sudoku.grid[0] = ['1','1','3','4','5','6','7','8','9'];
     sudoku.grid[1] = [,'1','1','4','5','6','7','8','9'];
-    expect(sudoku.validateRow(0)).toEqual(false);
-    expect(sudoku.validateRow(1)).toEqual(false);
+    expect(sudoku.validateCollection(sudoku.grid[0])).toEqual(false);
+    expect(sudoku.validateCollection(sudoku.grid[1])).toEqual(false);
   });
 });
   
--- src/sudoku.js
+++ src/sudoku.js
@@ -4,6 +4,8 @@ export default function Sudoku () {
   this.grid = Array.from(Array(9), () =>  new Array(9));
 }
 
-Sudoku.prototype.validateRow = function (arrayIndex) {
-
+Sudoku.prototype.validateCollection = function (arr) {
+  const definedArr = arr.filter(x => Boolean(x));
+  const arrayAsSet = new Set(definedArr);
+  return arrayAsSet.size === definedArr.length;
 }
