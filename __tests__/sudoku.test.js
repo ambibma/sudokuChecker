8b13862b78cb7c20da980e57696f93c93a635584
ambi hidalgo <ambio.pk@gmail.com>
2022-12-12 19:34:03 -0800

    1st test failed red
    Co-authored-by: Cameron Abel <cameronabel@gmail.com>
    Co-authored-by: Geoffrey Gao <geoffreygao1@gmail.com>

---
+++ __tests__/sudoku.test.js
@@ -0,0 +1,12 @@
+import Sudoku from "../src/sudoku";
+
+describe('Sudoku', () => {
+  test ('test it should a sudoku object that has an empty 2d array with 9 rows and 9 columns', () => {
+    const sudoku = new Sudoku();
+    expect(sudoku.grid.length).toEqual(9);
+    sudoku.grid.forEach(function(row) {
+      expect(row.length).toEqual(9);
+    });
+  });
+});
+  
