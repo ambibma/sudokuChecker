57eb25775c36fd5344ca2e3a4d0582c66951f0ca
ambi hidalgo <ambio.pk@gmail.com>
2022-12-13 19:05:06 -0800

    Test 4 & 5 failed expectedly
    Co-authored-by: Cameron Abel <cameronabel@gmail.com>
    Co-authored-by: Geoffrey Gao <geoffreygao1@gmail.com>

--- __tests__/sudoku.test.js
+++ __tests__/sudoku.test.js
@@ -5,6 +5,18 @@ describe('Sudoku', () => {
 
   beforeEach(() => {
     sudoku = new Sudoku();
+    // confirmed valid sudoku grid
+    sudoku.grid = [
+      ['1', '2', '3', '4', '5', '6', '7', '8', '9'],
+      ['7', '8', '9', '1', '2', '3', '4', '5', '6'],
+      ['4', '5', '6', '7', '8', '9', '1', '2', '3'],
+      ['3', '1', '2', '8', '4', '5', '9', '6', '7'],
+      ['6', '9', '7', '3', '1', '2', '8', '4', '5'],
+      ['8', '4', '5', '6', '9', '7', '3', '1', '2'],
+      ['2', '3', '1', '5', '7', '4', '6', '9', '8'],
+      ['9', '6', '8', '2', '3', '1', '5', '7', '4'],
+      ['5', '7', '4', '9', '6', '8', '2', '3', '1'],
+    ]
   });
 
   test ('it should return a sudoku object that has an empty 2d array with 9 rows and 9 columns', () => {
@@ -15,17 +27,23 @@ describe('Sudoku', () => {
   });
 
   test('it should return true if the given array does not contain duplicate numbers', () =>{ 
-    sudoku.grid[0] = ['1','2','3','4','5','6','7','8','9'];
-    sudoku.grid[1] = [,'1','3','4','5','6','7','8','9'];
-    expect(sudoku.validateCollection(sudoku.grid[0])).toEqual(true);
+    sudoku.grid[1] = ['1', '3', '4', '5', '6', '7', '8', '9'];
     expect(sudoku.validateCollection(sudoku.grid[1])).toEqual(true);
   });
 
   test('it should return false if the given array does contain duplicate numbers', () =>{ 
-    sudoku.grid[0] = ['1','1','3','4','5','6','7','8','9'];
-    sudoku.grid[1] = [,'1','1','4','5','6','7','8','9'];
+    sudoku.grid[0] = ['1', '1', '3', '4', '5', '6', '7', '8', '9'];
+    sudoku.grid[1] = [, '1', '1', '4', '5', '6', '7', '8', '9'];
     expect(sudoku.validateCollection(sudoku.grid[0])).toEqual(false);
     expect(sudoku.validateCollection(sudoku.grid[1])).toEqual(false);
   });
+  test('it should return true if the given grid does not contain rows with duplicate numbers', () =>{
+    expect(sudoku.validateRows()).toEqual(true);
+  });
+  test('it should return false if the given grid does contain rows with duplicate numbers', () =>{
+    sudoku.grid[0] = ['1', '1', '3', '4', '5', '6', '7', '8', '9'];
+    expect(sudoku.validateRows()).toEqual(false);
+  });
 });
+
   
--- src/sudoku.js
+++ src/sudoku.js
@@ -9,3 +9,35 @@ Sudoku.prototype.validateCollection = function (arr) {
   const arrayAsSet = new Set(definedArr);
   return arrayAsSet.size === definedArr.length;
 }
+
+Sudoku.prototype.validateRows = function () {
+  
+}
+
+
+
+
+
+
+
+
+
+
+// if (value) (this is the Boolean(x))
+
+// [ ,'1',,'1',,'6','7','8','9'];
+// "truth table" [false, true, false, true, false, true, true, true, true]
+
+// ['1', '1', '6', '7', '8', '9'].length = 6
+// {'1', '6', '7', '8', '9'}.size = 5
+
+
+// const definedArr = arr.filter(x => Boolean(x));
+
+// const definedArr = [];
+
+// arr.forEach( function(element) {
+//   if (element !== undefined) {
+//     definedArr.push(element);
+//   }
+// })
