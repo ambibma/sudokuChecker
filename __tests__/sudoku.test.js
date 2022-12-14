d47513fc11e69ff29329d6ba0b80c9df1afca740
ambi hidalgo <ambio.pk@gmail.com>
2022-12-13 18:38:14 -0800

    Add a before each in describe(sudoku)
    Co-authored-by: Cameron Abel <cameronabel@gmail.com>
    Co-authored-by: Geoffrey Gao <geoffreygao1@gmail.com>”

--- __tests__/sudoku.test.js
+++ __tests__/sudoku.test.js
@@ -1,8 +1,13 @@
 import Sudoku from "../src/sudoku";
 
 describe('Sudoku', () => {
+  let sudoku;
+
+  beforeEach(() => {
+    sudoku = new Sudoku();
+  });
+
   test ('it should return a sudoku object that has an empty 2d array with 9 rows and 9 columns', () => {
-    const sudoku = new Sudoku();
     expect(sudoku.grid.length).toEqual(9);
     sudoku.grid.forEach(function(row) {
       expect(row.length).toEqual(9);
@@ -10,7 +15,6 @@ describe('Sudoku', () => {
   });
 
   test('it should return true if the given array does not contain duplicate numbers', () =>{ 
-    const sudoku = new Sudoku();
     sudoku.grid[0] = ['1','2','3','4','5','6','7','8','9'];
     sudoku.grid[1] = [,'1','3','4','5','6','7','8','9'];
     expect(sudoku.validateCollection(sudoku.grid[0])).toEqual(true);
@@ -18,7 +22,6 @@ describe('Sudoku', () => {
   });
 
   test('it should return false if the given array does contain duplicate numbers', () =>{ 
-    const sudoku = new Sudoku();
     sudoku.grid[0] = ['1','1','3','4','5','6','7','8','9'];
     sudoku.grid[1] = [,'1','1','4','5','6','7','8','9'];
     expect(sudoku.validateCollection(sudoku.grid[0])).toEqual(false);
